d332e7b2bc0ca64e513c7a95fe8b6bd5a67d1f3c
keisukeYamagishi <jam330157@gmail.com>
2017-09-11 18:56:51 +0900

    add progress ImageConver

--- progress.py
+++ progress.py
@@ -7,12 +7,6 @@ class Progress:
     def __init__(self):
         self.isStop = False
 
-    # @classmethod
-    # def shared(cls):
-    #     if cls._instance is None:
-    #         cls._instance = cls()
-    #     return cls._instance
-
     def ProgresBar(self):
         for num in range(0,5):
             sys.stdout.write('Waiting' + '.' *num + '\r')
